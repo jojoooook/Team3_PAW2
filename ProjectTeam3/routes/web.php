7f39381b24422216b84738508f3a9d8d0f34ea34
Kanadesys <147543197+Kanadesys@users.noreply.github.com>
2024-09-13 11:03:46 +0700

    Membuat halaman login

--- ProjectTeam3/routes/web.php
+++ ProjectTeam3/routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Rute untuk tampilan login
+Route::get('login', [AuthController::class, 'showLoginForm'])->name('login');
+
+// Rute untuk menangani proses login
+Route::post('login', [AuthController::class, 'login']);
